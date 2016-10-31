aa0cad80053de78f70db4ff2efe127f7cfa280dc
cxa123230 <cuneytgurcan@yahoo.com>
2016-10-31 14:34:22 -0500

    graphx

---
+++ src/main/scala/tutorial/GraphXTutorial9Builders.scala
@@ -0,0 +1,46 @@
+package tutorial
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by cxa123230 on 10/31/2016.
+  */
+object GraphXTutorial9Builders {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName(s"${this.getClass.getSimpleName}")
+      .master("local[2]")
+      .getOrCreate()
+    val sc = spark.sparkContext
+    Logger.getRootLogger().setLevel(Level.ERROR)
+
+    println("The most basic graph can be created by giving an array of vertices" +
+      "and and array of edges. Check Graph(users, relationships) in Tutorial0Builder.scala:")
+    val primitiveGraph = new GraphXTutorial0Builder().createIntToyGraph(sc);
+
+    println("Another option is to create a graph from edges:")
+    val arr: RDD[Edge[Int]] = sc.parallelize(Array(Edge(7L, 3L, 21), Edge(5L, 3L, 15),
+      Edge(2L, 5L, 10), Edge(7L, 5L, 35)))
+    val defaultVertexAttribute: Long = 1
+    val rdGraph= Graph.fromEdges(arr,defaultValue=defaultVertexAttribute)
+    println("Here are the created vertices.")
+    rdGraph.vertices.foreach(println)
+    println("We can also create a graph from tuples of vertices: ")
+    val re:RDD[(Long, Long)] = sc.parallelize(Array((7L, 3L), (5L, 3L),
+      (2L, 5L), (7L, 5L),(7L, 5L)))
+    val tupleGraph = Graph.fromEdgeTuples(re,defaultValue = 1,
+      uniqueEdges = Some(PartitionStrategy.RandomVertexCut))
+    tupleGraph.edges.foreach(println)
+    println("Note that the duplicate edge 7->5 is indicated with Edge(7,5,2)")
+
+    println("The last method is to use the graph loader class and give it an edge list:")
+    val graph = GraphLoader.edgeListFile(sc,"src/main/resources/dblpgraph.txt")
+
+    println("graph has "+graph.numVertices+" vertices and "+graph.numEdges+" edges")
+
+  }
+}
